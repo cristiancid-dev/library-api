e4fbe03d9fd5d3f4dd8db3c188cfa9f4ec621254
Cristian <ccidbe@gmail.com>
2026-09-13 17:00:31 +0200

    feat: Add 'Book' entry validations

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -7,12 +7,18 @@ public class Book {
     // A class representing a book
 
     private final String isbn;
-    private final String title;
-    private final LocalDate publicationDate;
+    private String title;
+    private LocalDate publicationDate;
     private Author author;
     private String category;
 
     public Book(String isbn, String title, LocalDate publicationDate, Author author, String category) {
+        if (isbn == null) {
+            throw new IllegalArgumentException("ISBN cannot be null");
+        }
+        if (isbn.length() != 13) {
+            throw new IllegalArgumentException("ISBN length must be 13 digits");
+        }
         this.isbn = isbn;
         this.title = title;
         this.publicationDate = publicationDate;
@@ -46,11 +52,44 @@ public class Book {
     // Setters
 
 
+    public void setTitle(String title) {
+        if (title == null) {
+            throw new IllegalArgumentException("title cannot be null");
+        }
+
+        title = title.trim();
+
+        if (title.isEmpty()) {
+            throw new IllegalArgumentException("title cannot be empty");
+        }
+
+        this.title = title;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        if (publicationDate == null) {
+            throw new IllegalArgumentException("publication date cannot be null");
+        }
+        this.publicationDate = publicationDate;
+    }
+
     public void setAuthor(Author author) {
+        if (author == null) {
+            throw new IllegalArgumentException("author cannot be null");
+        }
         this.author = author;
     }
 
     public void setCategory(String category) {
+        if (category == null) {
+            throw new IllegalArgumentException("category cannot be null");
+        }
+
+        category = category.trim();
+
+        if (category.isEmpty()) {
+            throw new IllegalArgumentException("category cannot be empty");
+        }
         this.category = category;
     }
 
